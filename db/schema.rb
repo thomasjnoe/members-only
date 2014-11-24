d6e9dfbe60b5bc0dd49586b1f2e1e00850591966
Thomas Noe <thomasjnoe@gmail.com>
2014-11-24 01:35:26 -0500

    Add sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124052208) do
+ActiveRecord::Schema.define(version: 20141124063330) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141124052208) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
